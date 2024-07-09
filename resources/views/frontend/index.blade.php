de1a77c96683ee4ff5824ea47dc1742d2511c12b
Aymen Leknouche <dev.aymenleknouche@gmail.com>
2024-07-09 23:11:30 +0200

    frontend

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,35 @@
+<main class="main">
+
+    <!--Start hero slider-->
+    @include('frontend.body.hero')
+    <!--End hero slider-->
+
+    <!--Start category slider-->
+    @include('frontend.body.category')
+    <!--End category slider-->
+
+    <!--Start banners-->
+    @include('frontend.body.banners')
+    <!--End banners-->
+
+    <!--Start Products Tabs-->
+    @include('frontend.body.products')
+    <!--End Products Tabs-->
+
+    <!--Start Best Sales-->
+    @include('frontend.body.bestsales')
+    <!--End Best Sales-->
+
+    {{-- Start Categories --}}
+    @include('frontend.body.categories')
+    {{-- End Categories --}}
+
+    <!--Start 4 columns-->
+    @include('frontend.body.columns')
+    <!--End 4 columns-->
+
+    <!--Vendor List -->
+    @include('frontend.body.vendor')
+    <!--End Vendor List -->
+
+</main>
